ea2a4bd56b4f4d971b76044a0ab26c9bd5598d2e
Caio Beraldi Ribeiro <caio_4719@hotmail.com>
2023-02-12 20:48:21 -0300

    Configuração do Flutter Modular e criação de rotas

--- app/lib/src/app_widget.dart
+++ app/lib/src/app_widget.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
-import 'package:listinha/src/home/home_page.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 import 'package:listinha/src/shared/themes/themes.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    Modular.setInitialRoute('/home/');
+    return MaterialApp.router(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.dark,
       theme: lightTheme,
       darkTheme: darkTheme,
-      home: const HomePage(),
+      routerDelegate: Modular.routerDelegate,
+      routeInformationParser: Modular.routeInformationParser,
     );
   }
 }
